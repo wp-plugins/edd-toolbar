6c3432877fd3cb3d29613b4f496cd51e2a347665
daveshine <daveshine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-08 19:03:14 +0000

    Tagged version 1.4.3 of the plugin - maintenance & bugfix release... :)
    git-svn-id: https://plugins.svn.wordpress.org/edd-toolbar/tags/1.4.3@635891 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/index.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Do not put custom translations here. They will be deleted on 'Easy Digital Downloads Toolbar' updates!
+ *
+ * Keep custom 'Easy Digital Downloads Toolbar' translations in '/wp-content/languages/edd-toolbar/'
+ */
